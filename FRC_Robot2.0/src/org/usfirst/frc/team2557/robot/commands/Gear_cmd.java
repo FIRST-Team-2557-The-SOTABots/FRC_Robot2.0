379f637a3eb6f7aa8b037c53c51a96fd2475c2ea
Admin <Admin@10.237.81.207>
2017-02-21 15:18:05 -0800

    Now the gearAuto is commented out. :/

--- FRC_Robot2.0/src/org/usfirst/frc/team2557/robot/commands/Gear_cmd.java
+++ FRC_Robot2.0/src/org/usfirst/frc/team2557/robot/commands/Gear_cmd.java
@@ -21,9 +21,9 @@ public class Gear_cmd extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-//    	Robot.gear.gearGrab();
-//    	Robot.gear.gearPosition();
-    	Robot.gear.autoGear();
+    	Robot.gear.gearGrab();
+    	Robot.gear.gearPosition();
+//    	Robot.gear.autoGear();
     }
 
     // Make this return true when this Command no longer needs to run execute()
