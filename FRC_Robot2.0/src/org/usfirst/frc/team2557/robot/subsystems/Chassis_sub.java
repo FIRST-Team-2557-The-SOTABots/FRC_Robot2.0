7e138cb0516b124e87ccf7adb22455a3770d781a
Harrison <harri@10.237.81.154>
2018-01-22 10:56:01 -0800

    Undid some changes I did involuing solenoids

--- FRC_Robot2.0/src/org/usfirst/frc/team2557/robot/subsystems/Chassis_sub.java
+++ FRC_Robot2.0/src/org/usfirst/frc/team2557/robot/subsystems/Chassis_sub.java
@@ -14,102 +14,106 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Chassis_sub extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
 	double x = 1;
-	
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-    	setDefaultCommand(new Chassis_cmd());
-    }
-    
-    
-    public void butterflyDrive() {
-    	if(RobotMap.shift) {
-    		RobotMap.robotDrive.tankDrive(-Robot.oi.gamepad1.getRawAxis(1), -Robot.oi.gamepad1.getRawAxis(5));
-    	}
-    	else {
-    		RobotMap.mecDrive.driveCartesian(Robot.oi.gamepad1.getRawAxis(0), -Robot.oi.gamepad1.getRawAxis(1),
-    				Robot.oi.gamepad1.getRawAxis(4));
-    	}
-    }
-    
-    public void arcadeDrive(){
-    	if(RobotMap.drive){
-	    	RobotMap.robotDrive.arcadeDrive(Robot.oi.gamepad1.getRawAxis(1) *x, Robot.oi.gamepad1.getRawAxis(0) *.8);
-	    }
-	    else if(RobotMap.drive == false){
-	    	RobotMap.robotDrive.arcadeDrive(-Robot.oi.gamepad1.getRawAxis(1) *x, Robot.oi.gamepad1.getRawAxis(0) *.8);
-	    }
-    }
-    public void gemini(){
-    	if(Robot.oi.gamepad2.getPOV() == 0){
-    		RobotMap._gemini = true;
-    		return;
-    	}
-    	else if(Robot.oi.gamepad2.getPOV() == 180){
-    		RobotMap._gemini = false;
-    		return;
-    	}
-    	
-    	if(Robot.oi.a1.get()){
-    		RobotMap.drive = true;
-    		return;
-    	}
-    	else if(Robot.oi.b1.get()){
-    		RobotMap.drive = false;
-    		return;
-    	}
-    }
-    
-    public void distanceDrive(double _distance, double _power){
-    	if(RobotMap.euler.getDistance() < _distance){
-    		RobotMap.robotDrive.arcadeDrive(_distance, -_power);
-    	}
-    	else{
-    		RobotMap.robotDrive.arcadeDrive(0,0);
-    	}
-    }
-    
-    private double gyroOffset = 0;
-    public void resetDriveStraight(){
-    	gyroOffset = RobotMap.navX.getAngle();
-    }
-    public void driveStraight(double speed){
-    	RobotMap.robotDrive.arcadeDrive(speed, -(RobotMap.navX.getAngle() - gyroOffset) * 0.2);
-    }
-    public double getDriveStraightAngle() {
-    	return RobotMap.navX.getAngle() - gyroOffset;
-    }
 
-    private double _power, _turn1, _turn2;
-    public void encoderDrive(double speed){
-    	_power = speed;
-    	_turn1 = 0;
-    	_turn2 = 0;
-    	if(-RobotMap.FRdrive.getSensorCollection().getQuadratureVelocity() > RobotMap.FLdrive.getSensorCollection().getQuadratureVelocity()){
-    		_turn1 += .0002;
-    	}
-    	else if(-RobotMap.FRdrive.getSensorCollection().getQuadratureVelocity() < RobotMap.FLdrive.getSensorCollection().getQuadratureVelocity()){
-    		_turn1 -= .0002;
-    	}
-    	else{
-    		_turn1 += 0;
-    	}
-    	
-    	if(RobotMap.navX.getAngle() > 1){
-    		_turn2 += .00075;
-    	}
-    	else if(RobotMap.navX.getAngle() < -1){
-    		_turn2 -= .00075;
-    		
-    	}
-    	else{
-    		_turn2 += 0;
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		setDefaultCommand(new Chassis_cmd());
+	}
 
-    	}
-    	RobotMap.robotDrive.arcadeDrive(_power, _turn2); // _turn1 + _turn2);
-    	
-    }
+	public void butterflyDrive() {
+		if (RobotMap.shift) {
+			RobotMap.robotDrive.tankDrive(-Robot.oi.gamepad1.getRawAxis(1), -Robot.oi.gamepad1.getRawAxis(5));
+		} else {
+			RobotMap.mecDrive.driveCartesian(Robot.oi.gamepad1.getRawAxis(0), -Robot.oi.gamepad1.getRawAxis(1),
+					Robot.oi.gamepad1.getRawAxis(4));
+		}
+	}
+
+	public void arcadeDrive() {
+		if (RobotMap.drive) {
+			RobotMap.robotDrive.arcadeDrive(Robot.oi.gamepad1.getRawAxis(1) * x, Robot.oi.gamepad1.getRawAxis(0) * .8);
+		} else if (RobotMap.drive == false) {
+			RobotMap.robotDrive.arcadeDrive(-Robot.oi.gamepad1.getRawAxis(1) * x, Robot.oi.gamepad1.getRawAxis(0) * .8);
+		}
+	}
+
+	public void gemini() {
+		if (Robot.oi.gamepad2.getPOV() == 0) {
+			RobotMap._gemini = true;
+			return;
+		} else if (Robot.oi.gamepad2.getPOV() == 180) {
+			RobotMap._gemini = false;
+			return;
+		}
+
+		if (Robot.oi.a1.get()) {
+			RobotMap.drive = true;
+			return;
+		} else if (Robot.oi.b1.get()) {
+			RobotMap.drive = false;
+			return;
+		}
+	}
+
+	public void shift_toggle() {
+		if (RobotMap.shift) {
+			RobotMap.shifter.set(Value.kForward);
+		} else {
+			RobotMap.shifter.set(Value.kReverse);
+		}
+	}
+
+	public void distanceDrive(double _distance, double _power) {
+		if (RobotMap.euler.getDistance() < _distance) {
+			RobotMap.robotDrive.arcadeDrive(_distance, -_power);
+		} else {
+			RobotMap.robotDrive.arcadeDrive(0, 0);
+		}
+	}
+
+	private double gyroOffset = 0;
+
+	public void resetDriveStraight() {
+		gyroOffset = RobotMap.navX.getAngle();
+	}
+
+	public void driveStraight(double speed) {
+		RobotMap.robotDrive.arcadeDrive(speed, -(RobotMap.navX.getAngle() - gyroOffset) * 0.2);
+	}
+
+	public double getDriveStraightAngle() {
+		return RobotMap.navX.getAngle() - gyroOffset;
+	}
+
+	private double _power, _turn1, _turn2;
+
+	public void encoderDrive(double speed) {
+		_power = speed;
+		_turn1 = 0;
+		_turn2 = 0;
+		if (-RobotMap.FRdrive.getSensorCollection().getQuadratureVelocity() > RobotMap.FLdrive.getSensorCollection()
+				.getQuadratureVelocity()) {
+			_turn1 += .0002;
+		} else if (-RobotMap.FRdrive.getSensorCollection().getQuadratureVelocity() < RobotMap.FLdrive
+				.getSensorCollection().getQuadratureVelocity()) {
+			_turn1 -= .0002;
+		} else {
+			_turn1 += 0;
+		}
+
+		if (RobotMap.navX.getAngle() > 1) {
+			_turn2 += .00075;
+		} else if (RobotMap.navX.getAngle() < -1) {
+			_turn2 -= .00075;
+
+		} else {
+			_turn2 += 0;
+
+		}
+		RobotMap.robotDrive.arcadeDrive(_power, _turn2); // _turn1 + _turn2);
+
+	}
 }
-
