2891488be691f699de018b54fd5dcff3b4928ae4
Matt Miller <blacktop19@hotmail.com>
2018-02-20 19:28:59 -0800

    Add DISPLAY for X11.

--- pi/scripts/generic.sh
+++ pi/scripts/generic.sh
@@ -6,10 +6,13 @@
 COMMAND=
 
 # Check to see if the command is empty or not executable.
-if [ -z ${COMMAND} ] || [! -e ${COMMAND} ]; then
+if [ -z ${COMMAND} ] || [ ! -e ${COMMAND} ]; then
     echo "Command empty or not executable." 1>&2
     exit -1
 fi
+
+# If X11 is needed set the display variable.
+#DISPLAY=
 
 # Execute the command.
 exec ${COMMAND}
