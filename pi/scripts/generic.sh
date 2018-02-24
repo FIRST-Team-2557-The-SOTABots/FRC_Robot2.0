e6df1bee7d5800d4b632aa9c899919218d16581e
Matt Miller <blacktop19@hotmail.com>
2018-02-24 11:06:11 -0800

    Add comments on how to use the variables and give examples.

--- pi/scripts/generic.sh
+++ pi/scripts/generic.sh
@@ -2,14 +2,20 @@
 # Above is the shebang. The required line to tell what to translate the
 # script in
 
-# A variable to hold a command.
+# A variable to hold one command. Adding more than one command causes the
+# test below to fail.
+#COMMAND="/bin/ls"
 COMMAND=
 
 # Check to see if the command is empty or not executable.
-if [ -z ${COMMAND} ] || [ ! -e ${COMMAND} ]; then
+if [ -z "${COMMAND}" ] || [ ! -e "${COMMAND}" ]; then
     echo "Command empty or not executable." 1>&2
     exit -1
 fi
+
+# Add arguments to the command here.
+#COMMAND="${COMMAND} -la"
+COMMAND="${COMMAND}"
 
 # If X11 is needed set the display variable.
 #DISPLAY=
