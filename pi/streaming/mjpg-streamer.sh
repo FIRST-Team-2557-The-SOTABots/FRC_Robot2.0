5f9acc6058c5e92f1e80f101cde69e6ca47a4cc2
Matt Miller <blacktop19@hotmail.com>
2018-03-10 13:05:46 -0800

    Add startup scripts for Bishop's video streaming.

---
+++ pi/streaming/mjpg-streamer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Above is the shebang. The required line to tell what to translate the
+# script in
+
+# A variable to hold one command. Adding more than one command causes the
+# test below to fail.
+#COMMAND="/bin/ls"
+COMMAND="/opt/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer"
+
+# Check to see if the command is empty or not executable.
+if [ -z "${COMMAND}" ] || [ ! -e "${COMMAND}" ]; then
+    echo "Command empty or not executable." 1>&2
+    exit -1
+fi
+
+# Add arguments to the command here.
+#COMMAND="${COMMAND} -la"
+COMMAND="${COMMAND} -i \"input_uvc.so -d /dev/video1 -r 640x480 -f 20\" -o \"output_http.so -w ./www\""
+
+# If X11 is needed set the display variable.
+#DISPLAY=
+
+export LD_LIBRARY_PATH="/opt/mjpg-streamer/mjpg-streamer-experimental/"
+
+# Execute the command.
+exec ${COMMAND}
